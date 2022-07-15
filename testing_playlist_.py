c8edc3197236248bf91cdc91432106bdd8561480
Mitchell Chen <109147534+MitchellChen03@users.noreply.github.com>
2022-07-15 05:16:29 -0700

    Add files via upload

---
+++ testing_playlist_.py
@@ -0,0 +1,56 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import re
+import urllib.request
+
+from pytube import YouTube
+import os
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="20fd408cacad49bfacfa228e1183c3da",
+                                                           client_secret="1c9fb79aa2c14b9db4433b2d0475a4f7"))
+downloads = []
+loads=[]
+
+my_playlist = input("Please input the playlist link you wish to download: ")
+
+playlist_link = my_playlist
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+
+    album = track["track"]["album"]["name"]
+    print(album)
+    downloads.append(album)
+
+    my_songs = [item.replace(" ", "") for item in downloads]
+
+
+# takes the songs pulled from spodify artists into a list which is searched for on youtube which leaves us a link :)
+
+for i in my_songs:
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + i)
+    print(html)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    loads.append("https://www.youtube.com/watch?v=" + video_ids[0])
+
+
+
+
+
+# takes the list of youtube links, and goes on youtube to convert to mp3
+
+for load in downloads:
+    yt = YouTube(load)
+
+    video = yt.streams.filter(only_audio=True).first()
+
+    out_file = video.download(output_path=".")
+
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+
+print("done")
+
